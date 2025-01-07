dc2410f4897271bc2ccc94d80a5826caaa5cebc8
Mert Cotuk <b.mert.cotuk@gmail.com>
2025-01-07 14:24:07 +0300

    Initial commit

---
+++ src/main/java/com/bmcotuk/health_record_manager/HealthRecordManagerApplication.java
@@ -0,0 +1,13 @@
+package com.bmcotuk.health_record_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthRecordManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthRecordManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bmcotuk/health_record_manager/HealthRecordManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmcotuk.health_record_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthRecordManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
